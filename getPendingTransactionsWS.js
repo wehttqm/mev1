93a80802c082cf8cf2ec23ec6861de02658fec13
wehttqm <matthewfalcone5@gmail.com>
2024-01-14 21:00:45 -0500

    literally nothing has changed

--- getPendingTransactionsWS.js
+++ getPendingTransactionsWS.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.LOCAL_RPC_WS));
 
 const main = async () => {
-    const subscription = await web3.eth.subscribe('pendingTransactions', (err) => {
+    const subscription = await web3.eth.subscribe('newBlockHeaders', (err) => {
         if (err) {
             console.log(err)
         }
